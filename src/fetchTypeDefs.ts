4006294d122f28748bd3c6b2a4cd6b1ae574ba3f
nuesslerm <markus@choco.com>
2021-01-28 13:01:19 +0100

    presentation for automockedprovider 3

--- src/fetchTypeDefs.ts
+++ src/fetchTypeDefs.ts
@@ -1,9 +1,4 @@
-// import * as fs from 'fs';
-// import { printSchema, print } from 'graphql';
-// import fetch from 'isomorphic-unfetch';
-// import { introspectSchema } from '@graphql-tools/wrap';
-// import { AsyncExecutor } from '@graphql-tools/delegate';
-
+import fetch from 'isomorphic-unfetch';
 import buildAdminClient from './choco-appsync/buildAdminClient';
 import {
   buildClientSchema,
@@ -12,49 +7,32 @@ import {
   printSchema,
 } from 'graphql';
 
-// interface FetchTypeDefOptions {
-//   uri: string;
-//   typescript: boolean;
-//   path: string;
-// }
-
-// export const fetchTypeDefs = async ({
-//   uri = 'http://localhost:4000/graphql',
-//   typescript = true,
-//   path = `${process.cwd()}/typeDefs.${typescript ? 'ts' : 'js'}`,
-// }: FetchTypeDefOptions) => {
-//   console.log('writing typeDefs to: ', path);
-
-//   const executor: AsyncExecutor = async ({ document, variables }) => {
-//     const query = print(document);
-//     const fetchResult = await fetch(uri, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ query, variables }),
-//     });
-//     return fetchResult.json();
-//   };
-
-//   const schema = await introspectSchema(executor);
-
-//   fs.writeFileSync(
-//     path,
-//     `export const typeDefs = \`
-// ${printSchema(schema).replace(/`/g, '\\`')}\``
-//   );
-// };
-
-export const fetchTypeDefs = async () => {
+export const fetchTypeDefsWithUser = async () => {
   const chocoClient = await buildAdminClient();
   const introspectionResult: IntrospectionQuery = await chocoClient.request(
     `${getIntrospectionQuery()}`
   );
   const graphQLSchema = buildClientSchema(introspectionResult);
-  // console.log(chocoClient, graphQLSchema);
+  const typeDefs = printSchema(graphQLSchema);
 
-  const typeDefs = `${printSchema(graphQLSchema).replace(/`/g, '\\`')}`;
+  // fs.writeFileSync(
+  //   `${process.cwd()}/src/typeDefs.ts`,
+  //   `export const typeDefs = \`
+  // ${printSchema(graphQLSchema).replace(/`/g, '\\`')}\``
+  // );
+
+  return typeDefs;
+};
+
+export const fetchTypeDefs = async () => {
+  const uri =
+    'https://choco-doc.s3-eu-west-1.amazonaws.com/choco-appsync/gql_schema/schema.json';
+
+  const response = await fetch(uri);
+  const { data: jsonSchema } = await response.json();
+
+  const graphQLSchema = buildClientSchema(jsonSchema);
+  const typeDefs = printSchema(graphQLSchema);
 
   // fs.writeFileSync(
   //   `${process.cwd()}/src/typeDefs.ts`,
